de604b1038d5c9b0dde80b1ca876d7d90d852ce2
Mike Neville <mikenev@verily.com>
2022-11-18 10:11:09 -0500

    Addressed MR feedback. Added comments and style fixes.

--- service/src/main/java/bio/terra/policy/service/region/RegionService.java
+++ service/src/main/java/bio/terra/policy/service/region/RegionService.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +18,6 @@ import org.yaml.snakeyaml.constructor.Constructor;
 public class RegionService {
   private static final Logger logger = LoggerFactory.getLogger(RegionService.class);
 
-  private final Region ontology;
-
   private final HashMap<String, HashSet<String>> regionDatacenterMap;
   private final HashMap<String, Region> regionNameMap;
   private final HashMap<String, Datacenter> datacenterNameMap;
@@ -28,8 +27,14 @@ public class RegionService {
     Yaml regionYaml = new Yaml(new Constructor(Region.class));
     InputStream inputStream =
         this.getClass().getClassLoader().getResourceAsStream("static/regions.yml");
-    this.ontology = regionYaml.load(inputStream);
+    Region rootRegion = regionYaml.load(inputStream);
 
+    /**
+     * Snakeyaml can easily transform into java classes like Regions above. However, it can't
+     * naturally load into Java Collections. Datacenters.yml represents an array or List of
+     * Datacenters. Since we want to work with that data type, we first create an object of that
+     * type to indicate to snakeyaml how to load the file.
+     */
     Datacenter[] type = new Datacenter[0];
     Yaml datacenterYaml = new Yaml(new Constructor(type.getClass()));
     inputStream = this.getClass().getClassLoader().getResourceAsStream("static/datacenters.yml");
@@ -43,38 +48,44 @@ public class RegionService {
       this.datacenterNameMap.put(datacenter.getId(), datacenter);
     }
 
-    ConstructRegionMapsRecursively(this.ontology);
+    constructRegionMapsRecursively(rootRegion);
   }
 
-  public Boolean regionContainsDatacenter(String regionName, String datacenterId) {
+  public boolean regionContainsDatacenter(String regionName, String datacenterId) {
     return regionDatacenterMap.containsKey(regionName)
         && regionDatacenterMap.get(regionName).contains(datacenterId);
   }
 
+  @Nullable
   public Region getRegion(String name) {
-    return regionNameMap.containsKey(name) ? regionNameMap.get(name) : null;
+    return regionNameMap.get(name);
   }
 
+  @Nullable
   public Datacenter getDatacenter(String id) {
-    return this.datacenterNameMap.containsKey(id) ? datacenterNameMap.get(id) : null;
+    return datacenterNameMap.get(id);
   }
 
-  private void ConstructRegionMapsRecursively(Region current) {
+  private void constructRegionMapsRecursively(Region current) {
     if (current == null) return;
 
     regionNameMap.put(current.getName(), current);
-
     HashSet<String> currentDatacenters = new HashSet<>();
-
     String[] datacenters = current.getDatacenters();
+
+    // If there are no datacenters defined on the region in the regions.yml file,
+    // snakeyaml will set the value to null rather than populating it with an empty array.
     if (datacenters != null) {
       currentDatacenters.addAll(List.of(datacenters));
     }
 
     Region[] subregions = current.getRegions();
+
+    // Similarly, if there are no subregions defined in the .yml file, then this
+    // field will be null rather than an empty array.
     if (subregions != null) {
       for (Region subregion : current.getRegions()) {
-        ConstructRegionMapsRecursively(subregion);
+        constructRegionMapsRecursively(subregion);
         currentDatacenters.addAll(regionDatacenterMap.get(subregion.getName()));
       }
     }
