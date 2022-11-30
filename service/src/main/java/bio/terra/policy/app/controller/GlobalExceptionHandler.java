1ea8571eed39f5ce749a1ba3849bdeb5772cc711
Dan Dietterich <ddietterich@verily.com>
2022-11-30 11:31:10 -0500

    [PF-2266] Fix ErrorReport openapi and add the GlobalExceptionHandler (#30)
    * Fix ErrorReport openapi and add the GlobalExceptionHandler
    
    * Updates from code review
    
    * Spotless
    
    Co-authored-by: bumptagbot <robot@terra.team>

---
+++ service/src/main/java/bio/terra/policy/app/controller/GlobalExceptionHandler.java
@@ -0,0 +1,95 @@
+package bio.terra.policy.app.controller;
+
+import bio.terra.common.exception.ErrorReportException;
+import bio.terra.policy.generated.model.ApiErrorReport;
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.retry.backoff.BackOffInterruptedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+/**
+ * This module provides a top-level exception handler for controllers. All exceptions that rise
+ * through the controllers are caught in this handler. It converts the exceptions into standard
+ * ApiErrorReport responses.
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+  private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+  // -- Error Report - one of our exceptions --
+  @ExceptionHandler(ErrorReportException.class)
+  public ResponseEntity<ApiErrorReport> errorReportHandler(ErrorReportException ex) {
+    return buildApiErrorReport(ex, ex.getStatusCode(), ex.getCauses());
+  }
+
+  // -- validation exceptions - we don't control the exception raised
+  @ExceptionHandler({
+    MethodArgumentNotValidException.class,
+    MethodArgumentTypeMismatchException.class,
+    HttpMessageNotReadableException.class,
+    HttpRequestMethodNotSupportedException.class,
+    IllegalArgumentException.class,
+    NoHandlerFoundException.class
+  })
+  public ResponseEntity<ApiErrorReport> validationExceptionHandler(Exception ex) {
+    logger.error("Global exception handler: catch stack", ex);
+    // For security reasons, we generally don't want to include the user's invalid (and potentially
+    // malicious) input in the error response, which also means we don't include the full exception.
+    // Instead, we return a generic error message about input validation.
+    String validationErrorMessage =
+        "Request could not be parsed or was invalid: "
+            + ex.getClass().getSimpleName()
+            + ". Ensure that all types are correct and that enums have valid values.";
+    ApiErrorReport errorReport =
+        new ApiErrorReport()
+            .message(validationErrorMessage)
+            .statusCode(HttpStatus.BAD_REQUEST.value());
+    return new ResponseEntity<>(errorReport, HttpStatus.BAD_REQUEST);
+  }
+
+  // Exception thrown by Spring Retry code when interrupted.
+  @ExceptionHandler({BackOffInterruptedException.class})
+  public ResponseEntity<ApiErrorReport> retryBackoffExceptionHandler(
+      BackOffInterruptedException ex) {
+    String errorMessage =
+        "Unexpected interrupt while retrying internal logic. This may succeed on a retry. "
+            + ex.getMessage();
+    ApiErrorReport errorReport =
+        new ApiErrorReport()
+            .message(errorMessage)
+            .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    return new ResponseEntity<>(errorReport, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  // -- catchall - log so we can understand what we have missed in the handlers above
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ApiErrorReport> catchallHandler(Exception ex) {
+    logger.error("Exception caught by catchall handler", ex);
+    return buildApiErrorReport(ex, HttpStatus.INTERNAL_SERVER_ERROR, null);
+  }
+
+  private ResponseEntity<ApiErrorReport> buildApiErrorReport(
+      Throwable ex, HttpStatus statusCode, List<String> causes) {
+    StringBuilder combinedCauseString = new StringBuilder();
+    for (Throwable cause = ex; cause != null; cause = cause.getCause()) {
+      combinedCauseString.append("cause: ").append(cause).append(", ");
+    }
+    logger.error("Global exception handler: " + combinedCauseString, ex);
+    String message =
+        Optional.ofNullable(ex).map(Throwable::getMessage).orElse("no message present");
+
+    ApiErrorReport errorReport =
+        new ApiErrorReport().message(message).statusCode(statusCode.value()).causes(causes);
+    return new ResponseEntity<>(errorReport, statusCode);
+  }
+}
