a552e8477f49b207ba03e01de76d5c022fceb63a
Kai <kfeldman@broadinstitute.org>
2023-12-13 23:05:48 -0500

    wip creating contract tests for public api controller

--- service/src/test/java/bio/terra/policy/controller/VerifyPactsTpsPublicApiController.java
+++ service/src/test/java/bio/terra/policy/controller/VerifyPactsTpsPublicApiController.java
@@ -1,5 +1,9 @@
 package bio.terra.policy.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
@@ -8,6 +12,10 @@ import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
 import bio.terra.policy.app.controller.PublicApiController;
 import bio.terra.policy.app.controller.TpsApiController;
+import bio.terra.policy.generated.model.ApiTpsPolicyInput;
+import bio.terra.policy.generated.model.ApiTpsPolicyInputs;
+import bio.terra.policy.generated.model.ApiTpsPolicyPair;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestTemplate;
@@ -20,7 +28,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @Tag("Pact")
 @WebMvcTest
-@ContextConfiguration(classes = {PublicApiController.class})
+@ContextConfiguration(classes = {PublicApiController.class, MvcUtils.class})
 @Provider("tps")
 // @PactBroker()
 @PactFolder("pacts")
@@ -29,8 +37,25 @@ class VerifyPactsTpsPublicApiController {
   @MockBean private PublicApiController publicApiController;
   @MockBean private TpsApiController tpsApiController;
 
+  @Autowired private MvcUtils mvcUtils;
+
   // This mockMVC is what we use to test API requests and responses:
   @Autowired private MockMvc mockMvc;
+
+  private static final String TERRA = "terra";
+  private static final String GROUP_CONSTRAINT = "group-constraint";
+  private static final String REGION_CONSTRAINT = "region-constraint";
+  private static final String GROUP = "group";
+  private static final String REGION = "region-name";
+  private static final String DDGROUP = "ddgroup";
+  private static final String US_REGION = "usa";
+  private static final String IOWA_REGION = "iowa";
+
+  private String checkInitialLastUpdate(UUID paoId) throws Exception {
+    var apiPao = mvcUtils.getPao(paoId);
+    assertEquals(apiPao.getCreatedDate(), apiPao.getLastUpdatedDate());
+    return apiPao.getLastUpdatedDate();
+  }
 
   @TestTemplate
   @ExtendWith(PactVerificationSpringProvider.class)
@@ -43,6 +68,53 @@ class VerifyPactsTpsPublicApiController {
     context.setTarget(new MockMvcTestTarget(mockMvc));
   }
 
-  @State({"Tps is ok"})
-  public void checkStatus() throws Exception {}
+  /*
+  Current TPS Contract Testing States:
+
+  "Tps is ok"
+  "Tps is not ok"
+  "an existing policy" -> "one policy exists"
+  "another existing policy" -> "two policies exist"
+
+  */
+
+  @State({"tpsIsOk"})
+  public void statusIsOk() throws Exception {
+    this.mockMvc.perform(get("/status")).andExpect(status().isOk());
+  }
+
+  @State({"tpsIsNotOk"})
+  public void statusIsNotOk() throws Exception {}
+
+  @State({"a group policy exists"})
+  public void groupPolicyExists() throws Exception {
+
+    // create a policy limited by group
+    var groupPolicy =
+        new ApiTpsPolicyInput()
+            .namespace(TERRA)
+            .name(GROUP_CONSTRAINT)
+            .addAdditionalDataItem(new ApiTpsPolicyPair().key(GROUP).value(DDGROUP));
+
+    var inputs = new ApiTpsPolicyInputs().addInputsItem(groupPolicy);
+
+    // Create a PAO
+    UUID paoIdA = mvcUtils.createPao(inputs);
+    String lastUpdated = checkInitialLastUpdate(paoIdA);
+  }
+
+  @State({"a region policy exists"})
+  public void regionPolicyExists() throws Exception {
+    // create a policy limited by region
+    var regionPolicy =
+        new ApiTpsPolicyInput()
+            .namespace(TERRA)
+            .name(REGION_CONSTRAINT)
+            .addAdditionalDataItem(new ApiTpsPolicyPair().key(REGION).value(US_REGION));
+
+    var inputs = new ApiTpsPolicyInputs().addInputsItem(regionPolicy);
+
+    // Create a PAO
+    UUID paoIdA = mvcUtils.createPao(inputs);
+  }
 }
