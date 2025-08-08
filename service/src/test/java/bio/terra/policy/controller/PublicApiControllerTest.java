9c5ab01ece267b7ee19c3569f1a0e33d2570794f
David An <davidan@broadinstitute.org>
2025-08-08 09:33:34 -0400

    CORE-648, CORE-658: TCL 1.1.43 and Spring Boot 3.5.4 (#122)
    * TCL 1.1.42 and Spring Boot 3.4.5
    
    * mockbean -> mockitobean
    
    * TCL 1.1.43 and Spring Boot 3.5.4

--- service/src/test/java/bio/terra/policy/controller/PublicApiControllerTest.java
+++ service/src/test/java/bio/terra/policy/controller/PublicApiControllerTest.java
@@ -12,8 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,7 +24,7 @@ class PublicApiControllerTest extends TestBase {
 
   @Autowired private MockMvc mockMvc;
 
-  @MockBean private ApiVersionProperties versionProperties;
+  @MockitoBean private ApiVersionProperties versionProperties;
 
   @Test
   void testStatus() throws Exception {
